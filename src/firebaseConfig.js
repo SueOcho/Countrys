ae240056a144fd6e946dc2f813f0fa913b0cb2d8
Josue <yoshuai_i@hotmail.com>
2021-07-23 23:18:33 -0500

    .

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,15 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBqyAgfapjEmBQULii9ZSMFqFoVRWFAS9o",
-    authDomain: "valid-banner-298405.firebaseapp.com",
-    projectId: "valid-banner-298405",
-    storageBucket: "valid-banner-298405.appspot.com",
-    messagingSenderId: "274604659606",
-    appId: "1:274604659606:web:9269c4de08c8515e93c3b7"
+  apiKey: "AIzaSyACJoNB-yVpkR49lfl272B0tj-1UxO4De8",
+  authDomain: "web2final-1e8eb.firebaseapp.com",
+  projectId: "web2final-1e8eb",
+  storageBucket: "web2final-1e8eb.appspot.com",
+  messagingSenderId: "341239149264",
+  appId: "1:341239149264:web:a75adca02bfc127c5e64d5"
   }; 
       // Initialize Firebase
       const fb = firebase.initializeApp(firebaseConfig);
       // export const fire = firebase 
       export const auth =  fb.auth();
-      export const db =  fb.firestore();
